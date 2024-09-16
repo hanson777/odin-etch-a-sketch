963c7aae32563d954fc5c7b5a726ff75ae7d32b4
hanson777 <154561030+hanson777@users.noreply.github.com>
2024-09-16 09:47:37 -0700

    adjust slider styling and overall color scheme

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const containerDiv = document.querySelector(".container")
-const sliderDisplay = document.querySelector(".sliderdisplay")
-const slider = document.querySelector(".slider")
-let boxCount = 16
-createGrid(boxCount)
+const containerDiv = document.querySelector(".container");
+const sliderDisplay = document.querySelector(".sliderdisplay");
+const slider = document.querySelector(".slider");
+let boxCount = 16;
+createGrid(boxCount);
 
 
 slider.oninput = function (){
-    sliderDisplay.textContent = `${this.value} x ${this.value}`
-    boxCount = this.value
+    sliderDisplay.textContent = `${this.value} x ${this.value}`;
+    boxCount = this.value;
     setTimeout(() => {
-        clearGrid()
-        createGrid(boxCount)
-    }, "100")
+        clearGrid();
+        createGrid(boxCount);
+    }, "100");
 }
 
 
 function createGrid(boxCount){
-    dFrag = document.createDocumentFragment()
+    dFrag = document.createDocumentFragment();
     for(let i = 0; i < boxCount; i++){
         for(let j = 0; j < boxCount; j++){
-            const box = document.createElement("div")
-            box.style.width = (containerDiv.clientWidth / boxCount) + 'px'
-            box.style.height = (containerDiv.clientHeight / boxCount) + 'px'
-            box.className = "draw"
-            dFrag.appendChild(box)
+            const box = document.createElement("div");
+            box.style.width = (containerDiv.clientWidth / boxCount) + 'px';
+            box.style.height = (containerDiv.clientHeight / boxCount) + 'px';
+            box.className = "draw";
+            dFrag.appendChild(box);
         }
     }
-    containerDiv.appendChild(dFrag)
+    containerDiv.appendChild(dFrag);
 }
 
 function clearGrid(){
-    containerDiv.innerHTML = ""
+    containerDiv.innerHTML = "";
 }
 
 
 document.addEventListener("mouseover", event => { 
     if(event.target.classList.contains("draw")){
-        event.target.classList.add("color")
+        event.target.classList.add("color");
     }
-})
+});
 
 
 
