49cecd7b164fa8b5be854be747a348467df7f80b
hanson777 <154561030+hanson777@users.noreply.github.com>
2024-09-16 09:29:31 -0700

    Add a slider to change number of grids in canvas

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const containerDiv = document.querySelector(".container")
-let boxCount = 50
+const sliderDisplay = document.querySelector(".sliderdisplay")
+const slider = document.querySelector(".slider")
+let boxCount = 16
+createGrid(boxCount)
 
 
-for(let i = 0; i < boxCount; i++){
-    for(let j = 0; j < boxCount; j++){
-        const box = document.createElement("div")
-        box.style.width = (containerDiv.clientWidth / boxCount) + 'px'
-        box.style.height = (containerDiv.clientHeight / boxCount) + 'px'
-        box.className = "draw"
-        containerDiv.appendChild(box)
-    }
+slider.oninput = function (){
+    sliderDisplay.textContent = `${this.value} x ${this.value}`
+    boxCount = this.value
+    setTimeout(() => {
+        clearGrid()
+        createGrid(boxCount)
+    }, "100")
 }
+
+
+function createGrid(boxCount){
+    dFrag = document.createDocumentFragment()
+    for(let i = 0; i < boxCount; i++){
+        for(let j = 0; j < boxCount; j++){
+            const box = document.createElement("div")
+            box.style.width = (containerDiv.clientWidth / boxCount) + 'px'
+            box.style.height = (containerDiv.clientHeight / boxCount) + 'px'
+            box.className = "draw"
+            dFrag.appendChild(box)
+        }
+    }
+    containerDiv.appendChild(dFrag)
+}
+
+function clearGrid(){
+    containerDiv.innerHTML = ""
+}
+
 
 document.addEventListener("mouseover", event => { 
     if(event.target.classList.contains("draw")){
