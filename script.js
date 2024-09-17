6d260b8779ef9f1a9a338f3a904c886cea6ad3a5
hanson777 <154561030+hanson777@users.noreply.github.com>
2024-09-17 16:22:46 -0700

    Add Color and Rainbow buttons and functionality to switch between buttons

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ const sliderDisplay = document.querySelector(".sliderdisplay");
 const slider = document.querySelector(".slider");
 const eraseButton = document.querySelector(".eraser");
 const clearButton = document.querySelector(".clear");
+const rainbowButton = document.querySelector(".rainbow");
+const defaultButton = document.querySelector(".default");
 
-let boxCount = 16;
-let erase = false;
+const clickEvent = new Event("click");
+
+let defaultSelected = true;
+let eraserSelected = false;
+let rainbowSelected = false;
 let mousedown = false;
 let mousehold = false;
+let boxCount = 16;
 
 createGrid(boxCount);
+
 
 // FUNCTIONS
 
@@ -42,6 +49,21 @@ function clearGrid(){
     containerDiv.innerHTML = "";
 }
 
+function buttonSelect(buttonSelected, selectedButton, deselectOne, deselectOneButton, deselectTwo, deselectTwoButton){
+    // Select the current button     
+    buttonSelected = true;
+    selectedButton.style.backgroundColor = "#333";
+    selectedButton.style.color = "rgba(237,237,237,255)";
+    // Deselect the other button
+    deselectOne = false;
+    deselectOneButton.style.backgroundColor = "rgba(237,237,237,255)";
+    deselectOneButton.style.color = "#333";
+    // Deselect the other button
+    deselectTwo = false;
+    deselectTwoButton.style.backgroundColor = "rgba(237,237,237,255)";
+    deselectTwoButton.style.color = "#333";
+}
+
 // EVENT LISTENERS 
 
 document.addEventListener("mousedown", () => mousedown = true);
@@ -50,22 +72,21 @@ document.addEventListener("mouseup", () => mousedown = false);
 
 document.addEventListener("mousemove", (event) => {
     if(mousedown){
-        if(event.target.classList.contains("draw") && !erase){
+        if(event.target.classList.contains("draw") && !eraserSelected){
             event.target.classList.add("color");
-        } else if(event.target.classList.contains("draw") && erase){
+        } else if(event.target.classList.contains("draw") && eraserSelected){
             event.target.classList.remove("color");
         }
     }
 });
 
-eraseButton.addEventListener("click", () => {
-    erase = !erase;
-    if(erase){
-        eraseButton.style.backgroundColor = "#333";
-        eraseButton.style.color = "white";
-    } else {
-        eraseButton.style.backgroundColor = "rgba(237,237,237,255)";
-        eraseButton.style.color = "#333";
+document.addEventListener("click", (event) => {
+    if(event.target.classList.contains("eraser")){
+        buttonSelect(eraserSelected, eraseButton, defaultSelected, defaultButton, rainbowSelected, rainbowButton);
+    } else if(event.target.classList.contains("default")){
+        buttonSelect(defaultSelected, defaultButton, eraserSelected, eraseButton, rainbowSelected, rainbowButton);
+    } else if(event.target.classList.contains("rainbow")){
+        buttonSelect(rainbowSelected, rainbowButton, eraserSelected, eraseButton, defaultSelected, defaultButton);
     }
 });
 
